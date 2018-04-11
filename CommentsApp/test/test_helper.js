910b9630e9124be545f3a51a2344ca3a7a6287fe
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-11 11:18:09 -0300

    chore: Build the renderComponent function to render React components.

--- CommentsApp/test/test_helper.js
+++ CommentsApp/test/test_helper.js
@@ -1,5 +1,7 @@
 import jsdom from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDom from 'react-dom';
 
 // Set up test environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
@@ -7,6 +9,11 @@ global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+  return $(ReactDom.findDOMNode(componentInstance)); // produces HTML
+}
 
 // build helper for simulating events
 
