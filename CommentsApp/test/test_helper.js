718a6e399274f8127f657efb57331c6ecf40c074
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-11 11:36:18 -0300

    chore: Add the Provider for the rencerComponent function.

--- CommentsApp/test/test_helper.js
+++ CommentsApp/test/test_helper.js
@@ -2,6 +2,11 @@ import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDom from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 // Set up test environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
@@ -10,7 +15,11 @@ const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers)}>
+      <ComponentClass />
+    </Provider>
+  );
 
   return $(ReactDom.findDOMNode(componentInstance)); // produces HTML
 }
@@ -18,3 +27,4 @@ function renderComponent(ComponentClass) {
 // build helper for simulating events
 
 // Set up chai-jquery
+export { renderComponent, expect };
