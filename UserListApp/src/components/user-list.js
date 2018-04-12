3cce41f186bed23d7bfe1a63e26b26d5eb5a2aea
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-04-12 16:22:43 -0300

    feat: Add middleware to handle promises.

--- UserListApp/src/components/user-list.js
+++ UserListApp/src/components/user-list.js
@@ -11,14 +11,18 @@ class UserList extends Component {
     return (
       <div className="card card-block">
         <h4 className="card-title">{user.name}</h4>
-        <p className="card-text">Cheese Factory</p>
-        <a className="btn btn-primary">Email</a>
+        <p className="card-text">{user.company.name}</p>
+        <a className="btn btn-primary" href={user.website}>
+          Website
+        </a>
       </div>
     );
   }
 
   render() {
-    return <div className="user-list">{this.props.users.map(this.renderUser)}</div>;
+    return (
+      <div className="user-list">{this.props.users.map(this.renderUser)}</div>
+    );
   }
 }
 
