12bef30a89e44b135553c17d02e623ee3e3e7cf1
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-04-16 10:52:50 -0300

    feat: Add CORS to the server application.

--- Authentication/server/index.js
+++ Authentication/server/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost/auth');
 const app = express();
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
