a19d736f451059a880bf13dacb7382b400cc2be6
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-04-16 16:46:18 -0300

    chore: Changed the root message from the server.

--- Authentication/server/router.js
+++ Authentication/server/router.js
@@ -7,7 +7,9 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
-  app.get('/', requireAuth, (req, res) => res.send({ hi: 'there' }));
+  app.get('/', requireAuth, (req, res) =>
+    res.send({ message: 'Hello World!' })
+  );
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 };
