59edc47317d4e4ff748c58533500b5b874b54f88
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-04-16 14:31:17 -0300

    feat: Add the Sign up and Sign out links.

--- Authentication/client/src/components/header.js
+++ Authentication/client/src/components/header.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      // Show link to sign out
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">
+            Sign out
+          </Link>
+        </li>
+      );
+    } else {
+      // Show link to sign in or sign up
+      return [
+        <li className="nav-item">
+          <Link className="nav-link" to="/signin">
+            Sign in
+          </Link>
+        </li>,
+        <li className="nav-item">
+          <Link className="nav-link" to="/signup">
+            Sign up
+          </Link>
+        </li>
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li className="nav-item">Sign in</li>
-        </ul>
+        <Link to="/" className="navbar-brand">
+          Redux Auth
+        </Link>
+        <ul className="nav navbar-nav">{this.renderLinks()}</ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
