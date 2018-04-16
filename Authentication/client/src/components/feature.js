746e71ff25730dad31e1c6b23f49c47b4ba11db2
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-04-16 16:54:01 -0300

    feat: Add the action to fetch messages on the /feature route.

--- Authentication/client/src/components/feature.js
+++ Authentication/client/src/components/feature.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Feature extends Component {
-    render() {
-        return (
-            <div>This is a feature</div>
-        );
-    }
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
+  render() {
+    return <div>{this.props.message}</div>;
+  }
 }
 
-export default Feature;
+function mapStateToProps(state) {
+  return { message: state.auth.message };
+}
+
+export default connect(mapStateToProps, actions)(Feature);
