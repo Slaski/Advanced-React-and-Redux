cac5fe9a16aaf8c2deed805bfec0456e4c9ae4fe
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@gmail.com>
2018-04-16 15:16:46 -0300

    feat: Add the Signup component.

---
+++ Authentication/client/src/components/auth/signup.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+  handleFormSubmit({ email, password, confirmPassword }) {}
+
+  renderField(field) {
+    const {
+      meta: { touched, error }
+    } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
+    return (
+      <fieldset className={className}>
+        <label>{field.label}</label>
+        <input className="form-control" type={field.type} {...field.input} />
+        <div className="text-help">{touched ? error : ''}</div>
+      </fieldset>
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <Field
+          label="Email"
+          name="email"
+          component={this.renderField}
+          type="text"
+        />
+        <Field
+          label="Password"
+          name="password"
+          component={this.renderField}
+          type="password"
+        />
+        <Field
+          label="Confirm Password"
+          name="confirmPassword"
+          component={this.renderField}
+          type="password"
+        />
+        <button type="submit">Sign up</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'confirmPassword']
+})(Signup);
